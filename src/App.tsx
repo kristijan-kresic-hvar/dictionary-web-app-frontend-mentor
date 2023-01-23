9d5c976d5090b16a00dc90f28efb8ca3a0938763
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2023-01-23 17:43:53 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="font-mono">Hello there</div>
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          100: '#3A3A3A',
+          200: '#2D2D2D',
+          300: '#1F1F1F',
+          400: '#050505'
+        },
+        gray: {
+          100: '#FFFFFF',
+          200: '#050505',
+          300: '#3A3A3A',
+          400: '#838383'
+        },
+        purple: '#A445ED',
+        red: '#FF5252'
+      },
+      fontFamily: {
+        mono: ['Inconsolata', 'monospace'],
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Lora', 'serif']
+      }
+    },
+    plugins: []
+  }
+}
