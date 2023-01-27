60e06d2bfe8dd437d2ee5ef04da3bce050757ce9
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-01-27 20:39:13 +0100

    Added error states to search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, KeyboardEvent } from 'react'
+import React, { useRef, KeyboardEvent, useState } from 'react'
 import styles from './Search.module.scss'
 
 interface Props {
@@ -9,9 +9,19 @@ interface Props {
 const Search: React.FC<Props> = ({ onSearch, placeholder }) => {
   const searchRef = useRef<HTMLInputElement>(null)
 
+  const [hasError, setHasError] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>('')
+
   const handleSearch = (event: KeyboardEvent<HTMLInputElement>): void => {
     if (event.key === 'Enter') {
       event.preventDefault()
+
+      if (searchRef.current?.value === '') {
+        setHasError(true)
+        setErrorMessage('Whoops, can’t be empty…')
+        return
+      }
+
       onSearch(searchRef.current?.value ?? '')
     }
   }
@@ -23,16 +33,23 @@ const Search: React.FC<Props> = ({ onSearch, placeholder }) => {
       </label>
       <input
         ref={searchRef}
-        className={`${
-          styles.search as string
-        } w-full h-[3rem] sm:h-[4rem] bg-[#F4F4F4] dark:bg-[#1F1F1F] pl-[1.5rem] dark:text-white pr-[3rem] max-h-[64px] rounded-[1rem] focus:outline-none focus:ring-2 caret-purple font-bold focus:ring-purple focus:border-transparent`}
+        className={`${styles.search as string} w-full h-[3rem] sm:h-[4rem] ${
+          hasError ? 'ring-[#FF5252] ring-2' : 'focus:ring-purple focus:ring-2'
+        } bg-[#F4F4F4] dark:bg-[#1F1F1F] pl-[1.5rem] dark:text-white pr-[3rem] max-h-[64px] rounded-[1rem] focus:outline-none caret-purple font-bold focus:border-transparent`}
         id="search"
         type="text"
         placeholder={placeholder?.length != null ? placeholder : 'Search'}
         onKeyDown={(event) => {
+          setHasError(false)
+          setErrorMessage('')
           handleSearch(event)
         }}
       />
+      {hasError && (
+        <p className="text-[#FF5252] pt-[0.5rem] text-[0.9rem] leading-[1rem] sm:text-[1.25rem] sm:leading-[1.5rem]">
+          {errorMessage}
+        </p>
+      )}
     </form>
   )
 }
