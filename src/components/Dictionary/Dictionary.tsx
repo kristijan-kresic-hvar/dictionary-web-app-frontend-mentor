3dcb5f02420d7b495ccd68a1569266df1c73e56b
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-01-30 00:57:03 +0100

    Included synonyms

--- src/components/Dictionary/Dictionary.tsx
+++ src/components/Dictionary/Dictionary.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import { getWordDefinition } from '@/api'
 import { useQuery } from '@tanstack/react-query'
 
@@ -32,6 +33,8 @@ const Dictionary = ({ query }: DictionaryProps): JSX.Element => {
     (phonetic: any) => phonetic.audio.length > 0
   )?.audio
 
+  console.log(response.data[0])
+
   return (
     <div>
       <div className="flex justify-between items-center mb-[2.5rem]">
@@ -46,11 +49,27 @@ const Dictionary = ({ query }: DictionaryProps): JSX.Element => {
         {audioSrc?.length > 0 && <WordAudio audio={audioSrc} />}
       </div>
       {response.data[0].meanings.map((meaning: any, index: number) => (
-        <Meaning
-          key={index}
-          title={meaning.partOfSpeech}
-          list={meaning.definitions}
-        />
+        <Fragment key={index}>
+          <Meaning title={meaning.partOfSpeech} list={meaning.definitions} />
+          <div>
+            {meaning.synonyms?.length > 0 && (
+              <div className="mb-[2.5rem] flex flex-wrap w-full justify-start gap-[1.4rem] items-center">
+                <h3 className="font-[400] text-[1rem] sm:text-[1.25rem] text-[#757575]">
+                  Synonyms
+                </h3>
+                <div className="flex justify-start gap-[1.4rem] flex-wrap">
+                  {meaning.synonyms.map((synonym: string, index: number) => (
+                    <span
+                      key={index}
+                      className="text-purple font-bold text-[1rem] sm:text-[1.25rem]">
+                      {synonym}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </Fragment>
       ))}
       {response.data[0].sourceUrls.length > 0 && (
         <Source href={response.data[0].sourceUrls[0]} />
