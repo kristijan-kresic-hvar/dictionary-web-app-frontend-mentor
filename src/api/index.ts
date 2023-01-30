92228185293a4dbae51ddc908ee03913fd4f75e5
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2023-01-30 14:10:36 +0100

    Added timeout to fetch

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,15 @@
 import { API_URL } from '../constants'
 
 export const getWordDefinition = async (word: string): Promise<string> => {
-  console.log(word)
-  const response = await fetch(`${API_URL}${word}`)
+  const timeout: number = 5000
+  const controller: AbortController = new AbortController()
+  const timeoutId = setTimeout(() => {
+    controller.abort()
+  }, timeout)
+  const response = await fetch(`${API_URL}${word}`, {
+    signal: controller.signal
+  })
+  clearTimeout(timeoutId)
 
   if (!response.ok) {
     throw new Error('Failed to fetch word definition')
